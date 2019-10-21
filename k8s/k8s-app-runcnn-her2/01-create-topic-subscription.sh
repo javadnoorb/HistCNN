5feca72919244f072f2fc477e8fdcbd3d6413c41
Javad Noorbakhsh <javad.noorbakhsh@gmail.com>
2019-10-21 13:17:28 +0000

    copy new app

---
+++ k8s/k8s-app-runcnn-her2/01-create-topic-subscription.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+tissue="kidney"
+
+echo -e "\nThis script assumes the cluster $cluster_name in project $project_id is up and running."
+echo -e "If it's not the case please first create a cluster using ../01-create-k8s-cluster.sh"
+echo -e "\nThis script also assumes you have created the service key ../service-key.json"
+echo -e "If this is not true please first run ../02-set-service-account-once-per-project.sh"
+
+echo -e "\nLoading global variables from ../00-project-config.sh...\n"
+source ../00-project-config.sh
+
+if [ ! -e 00-app-config.sh ]; then
+  echo -e "\nError: 00-app-config.sh not found!"
+  echo -e "A template was created for you. Please complete it with your specific app values and try again.\n"
+  echo "#!/bin/bash" > 00-app-config.sh
+  echo -e "export input_bucket='histology-cnn'  # Bucket name will be used to get the tiles from" >> 00-app-config.sh
+  job_tag="tfrecords_runcnn_her2"
+  echo -e "export task_kind='Task:$job_tag'  # Task type for DataStore" >> 00-app-config.sh
+  echo -e "export topic_name='create_$job_tag'  # Topic for Pub/Sub" >> 00-app-config.sh
+  echo -e "export subscription_name='create_${job_tag}_sub'  # Subscription for Pub/Sub" >> 00-app-config.sh
+  echo -e "export image_name='create_$job_tag'  # Image name used for docker and Container Registry" >> 00-app-config.sh
+  echo -e "export deployment_name='${job_tag//_/-}'  # will be used for deployment, container, and pod." >> 00-app-config.sh
+  echo -e "export max_replica=13  # will be used for the max replica to be launched in deplyoment" >> 00-app-config.sh
+  echo -e "export ack_deadline_seconds=60  # Pub/Sub deadline from 0 to 600" >> 00-app-config.sh
+  echo -e "export annotations_path='data/pancancer_annotations/her2_cache_anns' #path of annotation files" >> 00-app-config.sh
+  echo -e "export results_path='data/run-results/her2/' # where to save the outputs of the run" >> 00-app-config.sh
+  echo -e "export pancancer_tfrecords_path='tfrecords/her2' # tfrecords path" >> 00-app-config.sh
+return
+fi
+
+source 00-app-config.sh
+echo -e "\nSetting up topic and subscription:"
+python pubsub-setup.py
+
+echo -e "\nListing all existing subscriptions:"
+gcloud pubsub subscriptions list
---
+++ k8s/k8s-app-runcnn-her2/publish.py
@@ -0,0 +1,72 @@
+from google.cloud import pubsub_v1 as pubsub
+from google.cloud import datastore
+from tqdm import tqdm
+import numpy as np
+import time
+import os
+from math import ceil
+
+project_id = os.environ['project_id']
+topic_name = os.environ['topic_name']
+task_kind = os.environ['task_kind']
+# saved_model_path = os.environ['saved_model_path']
+# tfrecpath = os.environ['tfrecpath']
+# image_file_metadata = os.environ['image_file_metadata']
+
+publisher = pubsub.PublisherClient()
+topic_path = publisher.topic_path(project_id, topic_name)
+
+def create_client(project_id):
+    return datastore.Client(project_id)
+client = create_client(project_id)
+
+def add_task(client, n, cancertype):
+    key = client.key(task_kind)
+
+    task = datastore.Entity(
+        key, exclude_from_indexes=[])
+
+    task.update({
+        'task_number': n,
+        'status': 'Queued',
+        'created': time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
+#         'gcs_output_path': gcs_output_path,
+#         'saved_model_path': saved_model_path,
+#         'image_file_metadata': image_file_metadata,
+#         'tfrecpath': tfrecpath,
+
+        'cancertype': cancertype,
+        'completed_time' : None,
+        'elapsed_time_s' : None,
+    })
+
+    client.put(task)
+
+    return task.key
+
+def list_tasks(client):
+    query = client.query(kind=task_kind)
+    query.order = ['created']
+
+    return list(query.fetch())
+
+def delete_task(client, task_id):
+    key = client.key('Task', task_id)
+    client.delete(key)
+
+
+cancertypes = ['blca', 'brca', 'chol', 'coad', 'esca', 'hnsc', 'kich', 'kirc', 'kirp',
+               'lihc', 'luad', 'lusc', 'ov', 'paad', 'prad', 'read', 'sarc', 'stad',
+               'thca', 'ucec']
+
+n = -1
+for cancertype in tqdm(cancertypes):
+    n+=1
+    # Creating a task list in DataStore
+    task = add_task(client, n, cancertype=cancertype)
+
+    # Publishing the task.id to Pub/Sub
+    data = str(task.id).encode('utf-8')
+    publisher.publish(topic_path, data)
+
+print('Published {:d} messages.'.format(n))
